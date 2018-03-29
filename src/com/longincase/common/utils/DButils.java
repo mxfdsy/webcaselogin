3b381c765e14e11a8378cc48a32d15b1dbf87f1b
963559210@qq.com <963559210@qq.com>
2018-03-29 17:25:04 +0800

    写完链接数据库

--- src/com/longincase/common/utils/DButils.java
+++ src/com/longincase/common/utils/DButils.java
@@ -1,8 +1,51 @@
 package com.longincase.common.utils;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by 没想法的岁月 on 2018/3/29.
  */
 public class DButils {
+    private static String driver = "com.mysql.jdbc.driver";
+    private static String url = "jdbc:mysql://localhost:3306/hello";
+    private static String user = "root";
+    private static String password = "cww963559210";
+
+    public static Connection getConn() throws SQLException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+        Connection conn  = dataSource.getConnection();
+        return conn;
+    }
+    public static  void close(ResultSet resultSet, PreparedStatement preparedStatement,Connection connection){
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (preparedStatement != null) {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
